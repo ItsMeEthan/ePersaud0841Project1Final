732d61261554592e6bcbcfb46af426d4959d3e91
ItsMeEthan <43207586+ItsMeEthan@users.noreply.github.com>
2018-10-08 13:27:52 -0400

    Delete EthanPersaudSmileyFaceFrownyFace.java

--- src/EthanPersaud13/EthanPersaudSmileyFaceFrownyFace.java
+++
@@ -1,9 +0,0 @@
-package EthanPersaud13;
-
-public class EthanPersaudSmileyFaceFrownyFace {
-    public static String areYouHappy(String s)
-    {
-        String y="the answer is your always happy because your great(trust me you can not say no)";
-        return y;
-    }
-}
